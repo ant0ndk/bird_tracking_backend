edcaa7d07940d7c0c15e9b7ead700be4eb5a96ec
Anton <kanton2001@yandex.ru>
2025-04-14 23:54:31 +0300

    configured cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,18 @@ from app.database import Base, engine
 from app.init_data import init_birds
 from app.routers import birds, sensor_data, sensor_versions, sensors
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Bird Tracking Service")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Создание таблиц при первом запуске
 Base.metadata.create_all(bind=engine)
